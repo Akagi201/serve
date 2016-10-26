e74daabfbf956acbccef230064f65400fd52ae58
Akagi201 <akagi201@gmail.com>
2016-10-26 14:56:57 +0800

    remove useless log

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 			log.Printf("error: %v\n", err.Error())
 			os.Exit(1)
 		} else {
-			log.Printf("%v\n", err.Error())
+			// log.Printf("%v\n", err.Error())
 			os.Exit(0)
 		}
 	}
