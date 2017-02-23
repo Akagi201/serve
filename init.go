bb3ed8709bf7575fe89b2bd3eb894e865354076b
Akagi201 <akagi201@gmail.com>
2017-02-23 17:28:12 +0800

    fix(waitgroup): fix http serve fail bug

--- init.go
+++ init.go
@@ -10,6 +10,18 @@ import (
 	"github.com/jessevdk/go-flags"
 )
 
+var opts struct {
+	LogLevel        string   `long:"log_level" default:"info" description:"log level"`
+	HTTPListenAddr  string   `long:"http" default:"0.0.0.0:80" description:"HTTP address to listen at, :0 to disable it"`
+	HTTPSListenAddr string   `long:"https" default:"0.0.0.0:443" description:"HTTPS address to listen at, :0 to disable it"`
+	HTTPSDomains    []string `long:"domains" description:"the allow domains, empty to allow all."`
+	HTML            string   `long:"html" default:"./" description:"the root html path to serve at"`
+	CacheFile       string   `long:"cache" default:"./letsencrypt.cache" description:"the cache for https."`
+	UseLetsEncrypt  bool     `long:"lets" description:"whether to use letsencrypt CA. self sign if not."`
+	SelfSignKey     string   `long:"ssk" default:"server.key" description:"self-sign key, user can build it: openssl genrsa -out server.key 2048"`
+	SelfSignCert    string   `long:"ssc" default:"server.crt" description:"self-sign cert, user can build it: openssl req -new -x509 -key server.key -out server.crt -days 365"`
+}
+
 func init() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 }
--- main.go
+++ main.go
@@ -14,18 +14,6 @@ import (
 	"github.com/ossrs/go-oryx-lib/https"
 )
 
-var opts struct {
-	LogLevel        string   `long:"log_level" default:"info" description:"log level"`
-	HTTPListenAddr  string   `long:"http" default:"0.0.0.0:80" description:"HTTP address to listen at, :0 to disable it"`
-	HTTPSListenAddr string   `long:"https" default:"0.0.0.0:443" description:"HTTPS address to listen at, :0 to disable it"`
-	HTTPSDomains    []string `long:"domains" description:"the allow domains, empty to allow all."`
-	HTML            string   `long:"html" default:"./" description:"the root html path to serve at"`
-	CacheFile       string   `long:"cache" default:"./letsencrypt.cache" description:"the cache for https."`
-	UseLetsEncrypt  bool     `long:"lets" description:"whether to use letsencrypt CA. self sign if not."`
-	SelfSignKey     string   `long:"ssk" default:"server.key" description:"self-sign key, user can build it: openssl genrsa -out server.key 2048"`
-	SelfSignCert    string   `long:"ssc" default:"server.crt" description:"self-sign cert, user can build it: openssl req -new -x509 -key server.key -out server.crt -days 365"`
-}
-
 func main() {
 	_, httpPort, err := net.SplitHostPort(opts.HTTPListenAddr)
 	if err != nil {
@@ -44,11 +32,12 @@ func main() {
 		log.Fatalln("http and https are both disabled")
 	}
 
-	var cacheFile string
-	var html string
+	cacheFile := opts.CacheFile
 	if !path.IsAbs(opts.CacheFile) && path.IsAbs(os.Args[0]) {
 		cacheFile = path.Join(path.Dir(os.Args[0]), opts.CacheFile)
 	}
+
+	html := opts.HTML
 	if !path.IsAbs(opts.HTML) && path.IsAbs(os.Args[0]) {
 		html = path.Join(path.Dir(os.Args[0]), opts.HTML)
 	}
@@ -123,6 +112,7 @@ func main() {
 			panic(err)
 		}
 	}()
+	wg.Add(1)
 
 	wg.Wait()
 }
